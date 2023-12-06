24f8914e3a0e5c8f12b51fcc6fcc10a428fe9bed
Ryan Zisk <ryan@ryanzisk.com>
2023-12-06 11:26:52 -0500

    day 2 part 1

---
+++ day02/day2.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/zisk/aoc2023/util"
+)
+
+type Game struct {
+	id      int
+	results []GameResult
+}
+
+type GameResult struct {
+	red, green, blue int
+}
+
+func NewGame(gamestring string) Game {
+	var newGame Game
+	initialSplit := strings.Split(gamestring, ":")
+
+	header := strings.Split(initialSplit[0], " ")
+	id, _ := strconv.Atoi(header[1])
+	newGame.id = id
+
+	drawStrings := strings.Split(initialSplit[1], ";")
+
+	parsedDraws := make([]GameResult, len(drawStrings))
+
+	i := 0
+	for _, d := range drawStrings {
+		parsedDraws[i] = ParseGameResult(d)
+		i++
+	}
+	newGame.results = parsedDraws
+	return newGame
+}
+
+func (g *Game) CheckGame() bool {
+	for _, r := range g.results {
+		if r.red > 12 || r.green > 13 || r.blue > 14 {
+			return false
+		}
+	}
+	return true
+}
+
+func ParseGameResult(resultString string) GameResult {
+	var result GameResult
+
+	resultSplit := strings.Split(resultString, ",")
+	for _, draw := range resultSplit {
+		s := strings.Split(strings.Trim(draw, " "), " ")
+		count, _ := strconv.Atoi(s[0])
+		switch s[1] {
+		case "red":
+			result.red = count
+		case "green":
+			result.green = count
+		case "blue":
+			result.blue = count
+		}
+	}
+
+	return result
+}
+
+func main() {
+	in, _ := util.InputToTxt()
+
+	//part 1
+	validCount := 0
+	for _, g := range in {
+		game := NewGame(g)
+		if game.CheckGame() {
+			validCount += game.id
+		}
+	}
+	fmt.Printf("Part 1: %d\n", validCount)
+}
